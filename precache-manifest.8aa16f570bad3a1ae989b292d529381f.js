69985f19e03be9ac994879e297d834d5d4f0c475
Samuel <coronado_samuel@hotmail.com>
2020-06-25 13:10:44 -0500

    Updates

---
+++ precache-manifest.8aa16f570bad3a1ae989b292d529381f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40dba4f479a4974a0e15e620b4a62872",
+    "url": "/CoronavirusTracker/index.html"
+  },
+  {
+    "revision": "02781651a9f4fe319f4a",
+    "url": "/CoronavirusTracker/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "aa7a06ab6b5132678e34",
+    "url": "/CoronavirusTracker/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "02781651a9f4fe319f4a",
+    "url": "/CoronavirusTracker/static/js/2.19bcbebc.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/CoronavirusTracker/static/js/2.19bcbebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa7a06ab6b5132678e34",
+    "url": "/CoronavirusTracker/static/js/main.501ba626.chunk.js"
+  },
+  {
+    "revision": "70d1a74faff462499135",
+    "url": "/CoronavirusTracker/static/js/runtime-main.be505493.js"
+  },
+  {
+    "revision": "5fe4039b11fa6d042519dd1d049901c9",
+    "url": "/CoronavirusTracker/static/media/loading.5fe4039b.gif"
+  }
+]);
